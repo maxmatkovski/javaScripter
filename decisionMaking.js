470ff4c2f17caf69f69b823ae6df91e64c5a6325
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-01 22:54:05 -0800

    Update decisionMaking.js

--- decisionMaking.js
+++ decisionMaking.js
@@ -13,3 +13,20 @@ days
 (7) ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 days.length
 7
+
+// Arrays and Array Length
+
+let colors = ["Red","Blue"]
+undefined
+colors [0] = "Yellow"
+"Yellow"
+colors
+(2) ["Yellow", "Blue"]
+colors [10]
+undefined
+colors [10] = "Purple"
+"Purple"
+colors
+(11) ["Yellow", "Blue", empty × 8, "Purple"]
+colors.length
+11
