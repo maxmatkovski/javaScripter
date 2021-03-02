ee132c927a18016fd4b7518ed47b374df3c234d7
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-01 22:48:47 -0800

    Update decisionMaking.js

--- decisionMaking.js
+++ decisionMaking.js
@@ -6,3 +6,10 @@ colors
 (2) ["hello", "friend"]
 colors [0]
 "hello"
+
+let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+undefined
+days
+(7) ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+days.length
+7
