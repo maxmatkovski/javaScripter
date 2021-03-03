a2036f69e5816e9da97e125f1d396f97fb3a57a4
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-02 16:41:45 -0800

    Update decisionMaking.js

--- decisionMaking.js
+++ decisionMaking.js
@@ -15,3 +15,30 @@ undefined
 age > 18
 true
 
+// Double vs Triple Equals
+undefined
+// Double Equals
+undefined
+1==1
+true
+1 == '1'
+true
+                 //  Double Equals Doesn't Care About Type
+
+
+// Triple Equals Accounts for Variable Type
+
+1 == true
+true
+0 == false
+true
+0 === false
+false
+1===true
+false
+1==='1'
+false
+
+
+
+
