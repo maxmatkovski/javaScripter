86902ec365504ab8fc858f9364347e7431e5097e
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-03 15:39:55 -0800

    Update logicalOperators.js

--- logicalOperators.js
+++ logicalOperators.js
@@ -5,3 +5,33 @@ if(!firstName){
 }
 
 // ! negates whatever it is applied to
+
+
+// Swtich Statement in JS
+
+const day = 0;
+switch(day){
+    case 1:
+        console.log("Monday")
+        break;
+    case 2:
+        console.log("Tuesday")
+        break;
+    case 3:
+        console.log("Wednesday")
+        break;
+    case 4:
+        console.log("Thursday")
+        break;
+    case 5:
+        console.log("Friday")
+        break;
+    case 6:
+        console.log("Saturday")
+        break;
+    case 7:
+        console.log("Sunday")
+        break;
+    default:
+        console.log("I don't know")
+}
