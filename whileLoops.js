72d00e22518160d2d2c52beb494e1e544ac53a3d
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-07 15:24:36 -0800

    Update whileLoops.js
    password checker

--- whileLoops.js
+++ whileLoops.js
@@ -4,3 +4,12 @@ while (count < 10){
     console.log(`Hello #${count + 1}`);
     count++;
 }
+
+// Mini Password Checker
+const SECRET = "BabyHippo";
+
+let guess = prompt("Enter the secret code: ");
+while(guess!== SECRET){
+    guess = prompt("Enter the secret code: ")
+}
+console.log("Congrats you got the secret!")
