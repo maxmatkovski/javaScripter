831e8c3b36633f5c4b54fd782fd174836485278b
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-07 15:14:45 -0800

    Update forLoops.js

--- forLoops.js
+++ forLoops.js
@@ -20,3 +20,17 @@ for (let i = 1; i <= 10; i++) {
         console.log(`        j is: ${j}`)
     }
 }
+
+const seatingChart = [
+    ['Kristen', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
+
+for (let i = 0; i < seatingChart.length; i++){
+    const row = seatingChart[i];
+    console.log(`ROW #${i + 1}`)
+    for(let j = 0; j < row.length; j++){
+        console.log(row[j])
+    }
+}
