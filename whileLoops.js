08c3cccc30cf74fd56e08ac8fabc77e09bd1c6ec
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-07 15:36:11 -0800

    Update whileLoops.js

--- whileLoops.js
+++ whileLoops.js
@@ -13,3 +13,20 @@ while(guess!== SECRET){
     guess = prompt("Enter the secret code: ")
 }
 console.log("Congrats you got the secret!")
+
+// Break
+let input = prompt("Hey say something: ")
+while (true){
+    input = prompt(input);
+    if(input.toLowerCase() === "stop copying me"){
+        break;
+    }
+}
+console.log("Ok You Win!")
+
+for (let i = 0; i < 1000; i++){
+    console.log(i);
+    if (i === 100){
+        break;
+    }
+}
