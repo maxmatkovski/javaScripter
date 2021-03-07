01aa21d01b320a0b5a593ff4fc4ba016564ccaa7
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-07 13:44:28 -0800

    Update forLoops.js

--- forLoops.js
+++ forLoops.js
@@ -8,3 +8,8 @@ for (let i = 1; i <= 6; i++){
 
 for (let i = 100; i >=0; i-=10){
     console.log(i)} // Make sure to use ';' not ',' to separate parameters
+
+// More Practice
+for (let i = 25; i >=0; i-=5){
+    console.log(i)
+}
