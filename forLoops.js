4699e11031679281efbdbfebefe21f492336d7c7
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-07 13:42:58 -0800

    Update forLoops.js
    more practice

--- forLoops.js
+++ forLoops.js
@@ -3,3 +3,8 @@
 for (let i = 1; i <= 6; i++){
     console.log("Da ba dee da ba daa")
 }
+
+// Another practice problem
+
+for (let i = 100; i >=0; i-=10){
+    console.log(i)} // Make sure to use ';' not ',' to separate parameters
