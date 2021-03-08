c684bc0349279ecf03fdd1213a5e49a611835db8
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-07 19:33:10 -0800

    Update forOfLoops.js

--- forOfLoops.js
+++ forOfLoops.js
@@ -11,3 +11,23 @@ for(let subreddit of subredditList){
 }
 
 // For of loops used when index isn't important and only used to iterate through an array
+
+
+const seatingChart = [
+    ['Kristen', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
+
+for (let i = 0; i < seatingChart.length; i++){
+    const row = seatingChart[i];
+    for (let j = 0; j < row.length; j++){
+        console.log(row[j])
+    }
+}
+
+for(let row of seatingChart){
+    for (let student of row){
+        console.log(student);
+    }
+}
