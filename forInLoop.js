fddd39978ec81ea89c25fbfd6018275e86d0f1c0
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-09 10:53:27 -0800

    Create forInLoop.js

---
+++ forInLoop.js
@@ -0,0 +1,35 @@
+const testScores = {
+    keenan: 80,
+    damon: 67,
+    kim: 89,
+    shawn: 91,
+    marlon: 72,
+    dwayne: 77,
+    nadia: 83,
+    elvira: 97,
+    diedre: 81,
+    vonnie: 60
+}
+
+// for(let person in testScores){
+//     console.log(person)
+// }
+
+// // For In Loop
+// for (let person in testScores){
+//     console.log(`${person} scored ${testScores[person]}`)
+// }
+
+// for(let person of Object.keys(testScores)){
+//     console.log(person)
+// }
+
+
+let total = 0;
+let scores = Object.values(testScores)
+for(let score of scores){
+    total += score; 
+}
+
+// Printing Average Score
+console.log(total/scores.length)
