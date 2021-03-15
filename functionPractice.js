a33c739855a95dbd013a7b6edcf7bba3d13937c5
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-14 18:49:02 -0700

    Create functionPractice.js
    lexical scope

---
+++ functionPractice.js
@@ -0,0 +1,21 @@
+// Block Scope
+for (let i = 0; i < 5; i++) {
+    let msg = 'aljkdlj';
+}
+console.log(msg);
+
+// Lexical Scope
+function bankRobbery (){
+    const heroes = ['Spiderman', 'Wolverine', 'Black Panther','BATWOMAN'];
+    function cryForHelp(){
+        function inner(){
+            for (let hero of heroes){
+                console.log(`PLEASE HELP US, ${hero.toUpperCase()}`)
+            }
+        }
+        inner ();
+    }
+    cryForHelp();
+}
+        // Notice how when using nested functions, it's important to call the function outside of itself
+
