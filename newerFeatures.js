5fae0f0d5ea58cacb20a8ce36cf583f5def79e58
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-23 00:27:22 -0700

    Update newerFeatures.js

--- newerFeatures.js
+++ newerFeatures.js
@@ -1,1 +1,16 @@
 
+// Default Parameters
+
+// Old Way of Setting Default Params
+
+function rollDie(numSides){
+    if(numSides === undefined){
+        numSides = 6;
+    }
+    return Math.floor(Math.random() * numSides) + 1;
+}
+
+// New / Better Way
+function rollDie(numSides = 6){
+    return Math.floor(Math.random() * numSides) + 1;
+}
