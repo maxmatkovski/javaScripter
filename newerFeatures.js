f3de92ba2b0b00faf69a8ac0116ac0a3c8edf1cf
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-23 00:35:09 -0700

    Update newerFeatures.js

--- newerFeatures.js
+++ newerFeatures.js
@@ -14,3 +14,14 @@ function rollDie(numSides){
 function rollDie(numSides = 6){
     return Math.floor(Math.random() * numSides) + 1;
 }
+
+// Another Example
+function greet(msg = 'Hey there', person){
+    console.log(`${msg}, ${person}!`)
+}
+
+// Need default parameter to be after the first
+
+function greet(person, msg = 'Hey there'){
+    console.log(`${msg}, ${person}!`)
+}
