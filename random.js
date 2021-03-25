4606145ef8de78de7156b6a1494cd50c36899cd2
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-25 14:36:39 -0700

    Create random.js
    Ternary Operator

---
+++ random.js
@@ -0,0 +1,16 @@
+// Ternary Operator
+
+// condition ? expr1 : expr 2
+
+    // Traditional If Statement
+let age = 15;
+
+if(age >= 18){
+    console.log('You are an adult')
+} else {
+    console.log('You are not an adult')
+};
+
+    // Teranary Operator
+let age = 15;
+console.log((age >= 18)? "You are an adult!" : "You are a kid.");
